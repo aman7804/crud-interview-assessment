93db50fdfee8e4df893307029d718f4d86083f77
aman <bhateriyaaman@gmail.com>
2024-07-24 18:29:59 +0530

    DB connection setup and basic crud contoller apis.

---
+++ crud-interview-assessment/crud-interview-assessment/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crud_interview_assessment
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options) { }
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
